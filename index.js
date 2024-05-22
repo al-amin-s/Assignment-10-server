956642bd1c15bfb6fbb1ef2ec68a68e980fe8243
Al Amin <alamin30204@gmail.com>
2024-05-22 10:37:21 +0600

    mycraft,allcraft and so on are added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
-const app=express();
-const port=process.env.PORT ||5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middlewere
 app.use(cors());
@@ -13,32 +13,57 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    
+        const artCollection = client.db("artDB").collection("art");
 
 
-    
 
-    // operation for sending data to localhost
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/art', async (req, res) => {
+            const newArt = req.body;
+            console.log(newArt);
+            const result = await artCollection.insertOne(newArt);
+            res.send(result)
+        })
+
+        app.get('/art', async (req, res) => {
+            const cursor = artCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // app.get('/art/:id', async (req, res) => {
+        //     const id=req.params.id;
+        //      const query={_id:new ObjectId(id)};
+        //     const result = await artCollection.findOne(query);
+        //     res.send(result);
+        // })
+
+        app.get("/art/:email", async(req,res)=>{
+            console.log(req.params.email);
+            const result=await artCollection.find({ email: req.params.email }). toArray();
+            res.send(result)
+        })
+        
+       
+        // operation for sending data to localhost
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -48,18 +73,18 @@ run().catch(console.dir);
 
 
 
-const hello=[
+const hello = [
     {
-        name:"alamin",
-        email:"ala756297@gmail.com",
-        mobile:"nai"
+        name: "alamin",
+        email: "ala756297@gmail.com",
+        mobile: "nai"
     }
 ]
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send(hello)
-} )
+})
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Example app are listening from ${port}`)
 })
